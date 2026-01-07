1f1724fd53f7b390364a2d9af61c8cfa15cef99c
sh95fit <sh95fit@gmail.com>
2026-01-07 14:11:57 +0900

    프로시저 오기 수정

--- services/business/backend/app/celery_app/config.py
+++ services/business/backend/app/celery_app/config.py
@@ -110,7 +110,7 @@ class CohortTaskConfig:
     LEAD_APPLICATIONS = {
         'spreadsheet_id': settings.google_sheet_id_cohort,
         'worksheet_name': "고객유입리드_DB",
-        'procedure_name': "call get_lead_applications_list",
+        'procedure_name': "get_lead_applications_list",
         'needs_target_date': False,
         'needs_date_header': False,    
         'needs_period': False,
